e4e0e6790fc23704a64e9b86b248aa18517ea3b1
Tri-Nguyen <tri.nguyen@coxautoinc.com>
2018-07-26 06:00:39 +0700

    2-Apply Prompt

--- HelloBot/Dialogs/RootDialog.cs
+++ HelloBot/Dialogs/RootDialog.cs
@@ -8,24 +8,38 @@ namespace HelloBot.Dialogs
     [Serializable]
     public class RootDialog : IDialog<object>
     {
-        public Task StartAsync(IDialogContext context)
+        public async Task StartAsync(IDialogContext context)
         {
             context.Wait(MessageReceivedAsync);
 
-            return Task.CompletedTask;
+            //return Task.CompletedTask;
         }
 
         private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
         {
             var activity = await result as Activity;
 
-            // Calculate something for us to return
-            int length = (activity.Text ?? string.Empty).Length;
+            //// Calculate something for us to return
+            //int length = (activity.Text ?? string.Empty).Length;
 
-            // Return our reply to the user
-            await context.PostAsync($"You sent {activity.Text} which was {length} characters");
+            //// Return our reply to the user
+            //await context.PostAsync($"You sent {activity.Text} which was {length} characters");
 
-            context.Wait(MessageReceivedAsync);
+            PromptDialog.Choice(
+                context,
+                AfterChoiceSelected,
+                new[] { "Messi", "Ronaldo" },
+                "Who is better player?",
+                "I am sorry but I did not understand that"
+                );
+
+            //context.Wait(MessageReceivedAsync);
+        }
+
+        private async Task AfterChoiceSelected(IDialogContext context, IAwaitable<string> result)
+        {
+            await context.PostAsync("Thanks for choosing: "+(await result));
+            await StartAsync(context);
         }
     }
 }
